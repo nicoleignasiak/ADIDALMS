e45356abf5bc5298856cd2dc71af44315475c472
Devin Simoneaux <devin.simoneaux@myemail.indwes.edu>
2018-04-20 01:43:59 -0400

    added new function in js file

--- scripting.js
+++ scripting.js
@@ -256,3 +256,13 @@ function bus()
 		busCount = 0;
 	}
 }
+
+function uploadOnChange()
+{
+	var filename = this.value;
+	var lastIndex = filename.lastIndexOf("\\");
+	if (lastIndex >= 0) {
+		filename = filename.substring(lastIndex + 1);
+	}
+	document.getElementById('filename').value = "Upload " + filename;
+}
